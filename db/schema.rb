942e7dedba9644f5a627784bd03c7f867578560d
Turan Sadigova <103653006+trimmtrabb@users.noreply.github.com>
2023-03-07 10:53:08 +0400

    Add sales_count to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_03_084135) do
+ActiveRecord::Schema.define(version: 2023_03_06_161617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_03_03_084135) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "stripe_product_id"
     t.string "stripe_price_id"
+    t.integer "sales_count", default: 0, null: false
   end
 
 end
