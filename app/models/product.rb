3289049d65b51b36b180af37d77653ca4ef735d7
Turan Sadigova <103653006+trimmtrabb@users.noreply.github.com>
2023-03-09 12:51:17 +0400

    Ensure stripe-related columns always have a value
    Follow-up to https://github.com/trimmtrabb/shoplify/commit/0f193102fda42014b8b47a3ccbfd3be8d0784c5f

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   validates :name, :price, presence: true
+
+  after_create do
+    product = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: product, unit_amount: self.price * 100,
+                                 currency: 'usd')
+    update(stripe_product_id: product.id, stripe_price_id: price.id)
+  end
 end
