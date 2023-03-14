6c871cd0ce725ccc10e649f8bdec3e65c10a580a
Turan Sadigova <103653006+trimmtrabb@users.noreply.github.com>
2023-03-14 12:10:53 +0400

    Increment sales price without webhooks upon successful payment
    FIXME left to fix associated bug later

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -18,15 +18,10 @@ class CheckoutController < ApplicationController
   end
 
   def success
-    if params[:session_id].present?
-      @session_with_expand = Stripe::Checkout::Session.retrieve({
-        id: params[:session_id],
-        expand: ["line_items"] })
-      @session_with_expand.line_items.data.each do |line_item|
-        product = Product.find_by(stripe_product_id: line_item.price.product)
-      end
-    else
-      redirect_to cancel_url, alert: "No info to display"
+    session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ["line_items"]})
+    session_with_expand.line_items.data.each do |line_item|
+      product = Product.find_by(stripe_product_id: line_item.price.product)
+      product.increment!(:sales_count)
     end
   end
 
