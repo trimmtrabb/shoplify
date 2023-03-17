a946fbf975c183ead965ce39af147d689d9070b1
Turan Sadigova <103653006+trimmtrabb@users.noreply.github.com>
2023-03-17 14:01:32 +0400

    Create and assign new Stripe price when price is updated in database

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,13 @@ class Product < ApplicationRecord
     update(stripe_product_id: product.id, stripe_price_id: price.id)
   end
 
+  after_update :assign_new_stripe_price, if: :saved_change_to_price?
+  def assign_new_stripe_price
+    price = Stripe::Price.create(product: stripe_product_id, unit_amount: self.price * 100,
+                                 currency: 'usd')
+    update(stripe_price_id: price.id)
+  end
+
   def to_builder
     Jbuilder.new do |product|
       product.price stripe_price_id
