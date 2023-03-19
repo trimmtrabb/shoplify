30ce430ce6c89c8829099909ef8bc0540013fd02
Turan Sadigova <103653006+trimmtrabb@users.noreply.github.com>
2023-03-19 11:14:07 +0400

    Adapt prices to gem 'money-rails'

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,14 +5,14 @@ class Product < ApplicationRecord
 
   after_create do
     product = Stripe::Product.create(name: name)
-    price = Stripe::Price.create(product: product, unit_amount: self.price * 100,
+    price = Stripe::Price.create(product: product, unit_amount: self.price,
                                  currency: currency)
     update(stripe_product_id: product.id, stripe_price_id: price.id)
   end
 
   after_update :assign_new_stripe_price, if: :saved_change_to_price?
   def assign_new_stripe_price
-    price = Stripe::Price.create(product: stripe_product_id, unit_amount: self.price * 100,
+    price = Stripe::Price.create(product: stripe_product_id, unit_amount: self.price,
                                  currency: currency)
     update(stripe_price_id: price.id)
   end
